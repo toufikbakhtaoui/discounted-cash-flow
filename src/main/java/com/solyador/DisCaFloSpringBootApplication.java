7517a4ba558af1134e62d6d4cedbf1d03a3799e2
toufikbakhtaoui <toufik.bakhtaoui@gmail.com>
2023-12-09 22:41:26 +0100

    Add pom and spring boot

---
+++ src/main/java/com/solyador/DisCaFloSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.solyador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisCaFloSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisCaFloSpringBootApplication.class, args);
+    }
+}
