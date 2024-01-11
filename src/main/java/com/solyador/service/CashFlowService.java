1507d9b4f88d81947b7102be087cb1340a7cd37e
toufikbakhtaoui <toufik.bakhtaoui@gmail.com>
2024-01-11 23:38:16 +0100

    add restTemplate

--- src/main/java/com/solyador/service/CashFlowService.java
+++ src/main/java/com/solyador/service/CashFlowService.java
@@ -2,6 +2,7 @@ package com.solyador.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -13,10 +14,17 @@ import java.net.URL;
 public class CashFlowService {
 
     private static final String API_KEY = "ZG4AL4Q0N9XBO";
+    private final RestTemplate restTemplate;
+
+    public CashFlowService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public void getCashFlow(String symbol) {
         try {
-            URL url = new URL("https://www.alphavantage.co/query?function=CASH_FLOW&symbol=Œ" + symbol + "&apikey=" + API_KEY);
+            var uri = "https://www.alphavantage.co/query?function=CASH_FLOW&symbol=Œ" + symbol + "&apikey=" + API_KEY;
+            var response = restTemplate.getForObject(uri, Object.class);
+            /*URL url = new URL();
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
@@ -27,10 +35,10 @@ public class CashFlowService {
                 response.append(line);
             }
             reader.close();
-
+            connection.disconnect();
+*/
             log.debug("Cash Flow Data:\n" + response);
 
-            connection.disconnect();
         } catch (Exception e) {
             log.error("error while trying to get cash flow");
         }
